5a01ceb50f362a3924669ae71e8de2af05af4757
cmarkstaller <cmarkstaller@gmail.com>
2025-02-06 09:37:49 -0700

    milestone one done

---
+++ tweeter-web/src/components/authentication/AuthenticationFields.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+interface Props {
+  action_function: (event: React.KeyboardEvent<HTMLElement>) => void;
+  set_alias: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  set_password: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const AuthenticationFields = (props: Props) => {
+  return (
+    <>
+      <div className="form-floating">
+        <input
+          type="text"
+          className="form-control"
+          size={50}
+          id="aliasInput"
+          placeholder="name@example.com"
+          onKeyDown={props.action_function}
+          onChange={props.set_alias}
+        />
+        <label htmlFor="aliasInput">Alias</label>
+      </div>
+      <div className="form-floating mb-3">
+        <input
+          type="password"
+          className="form-control bottom"
+          id="passwordInput"
+          placeholder="Password"
+          onKeyDown={props.action_function}
+          onChange={props.set_password}
+        />
+        <label htmlFor="passwordInput">Password</label>
+      </div>
+    </>
+  );
+};
+export default AuthenticationFields;
