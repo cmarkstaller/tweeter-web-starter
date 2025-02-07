5da180218da9521eb0ee6388a28cf0c698184c83
cmarkstaller <cmarkstaller@gmail.com>
2025-02-07 12:44:14 -0700

    Post Status tsx has been changed to mvp model

--- tweeter-web/src/components/postStatus/PostStatus.tsx
+++ tweeter-web/src/components/postStatus/PostStatus.tsx
@@ -1,48 +1,35 @@
 import "./PostStatus.css";
 import { useState } from "react";
-import { AuthToken, Status } from "tweeter-shared";
 import useToastListener from "../toaster/ToastListenerHook";
 import useUserInfo from "../userInfo/UserHook";
+import {
+  PostStatusPresenter,
+  PostStatusView,
+} from "../../presenters/PostStatusPresenter";
 
-const PostStatus = () => {
+interface Props {
+  presenterGenerator: (view: PostStatusView) => PostStatusPresenter;
+}
+
+const PostStatus = (props: Props) => {
   const { displayErrorMessage, displayInfoMessage, clearLastInfoMessage } =
     useToastListener();
 
   const { currentUser, authToken } = useUserInfo();
   const [post, setPost] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+
+  const listener: PostStatusView = {
+    displayErrorMessage: displayErrorMessage,
+    displayInfoMessage: displayInfoMessage,
+    clearLastInfoMessage: clearLastInfoMessage,
+    setPost: setPost,
+  };
+
+  const [presenter] = useState(props.presenterGenerator(listener));
 
   const submitPost = async (event: React.MouseEvent) => {
     event.preventDefault();
-
-    try {
-      setIsLoading(true);
-      displayInfoMessage("Posting status...", 0);
-
-      const status = new Status(post, currentUser!, Date.now());
-
-      await postStatus(authToken!, status);
-
-      setPost("");
-      displayInfoMessage("Status posted!", 2000);
-    } catch (error) {
-      displayErrorMessage(
-        `Failed to post the status because of exception: ${error}`
-      );
-    } finally {
-      clearLastInfoMessage();
-      setIsLoading(false);
-    }
-  };
-
-  const postStatus = async (
-    authToken: AuthToken,
-    newStatus: Status
-  ): Promise<void> => {
-    // Pause so we can see the logging out message. Remove when connected to the server
-    await new Promise((f) => setTimeout(f, 2000));
-
-    // TODO: Call the server to post the status
+    presenter.submitPost(currentUser!, authToken!, post);
   };
 
   const clearPost = (event: React.MouseEvent) => {
@@ -55,7 +42,7 @@ const PostStatus = () => {
   };
 
   return (
-    <div className={isLoading ? "loading" : ""}>
+    <div className={presenter.isLoading ? "loading" : ""}>
       <form>
         <div className="form-group mb-3">
           <textarea
@@ -78,7 +65,7 @@ const PostStatus = () => {
             style={{ width: "8em" }}
             onClick={(event) => submitPost(event)}
           >
-            {isLoading ? (
+            {presenter.isLoading ? (
               <span
                 className="spinner-border spinner-border-sm"
                 role="status"
